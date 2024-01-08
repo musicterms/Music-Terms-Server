a3b17aa9164b95afabe894c220302873d3965928
AlphaBrate Music Terminology Library <155535486+musicterms@users.noreply.github.com>
2024-01-08 16:17:38 +0800

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -12,7 +12,7 @@ const firebaseConfig = {
     projectId: process.env.FIREBASE_PROJECT_ID,
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_API_ID,
+    appId: process.env.FIREBASE_APP_ID,
 };
 console.log(firebaseConfig);
 // Config from .env file (HIDDEN IN GITHUB)
